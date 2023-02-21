95ab9c1fbbd63e1bda44f46ff35192193552dd53
lparasca <lparasca@esagaming.com>
2023-02-21 13:44:54 +0200

    added animation class

--- src/scripts/animation.ts
+++ src/scripts/animation.ts
@@ -1,9 +1,37 @@
+import * as PIXI from 'pixi.js';
 
 
-export default class animation {
+
+
+export default class Animation {
     constructor (){
-        
+       
     }
 
+    playNormalWinAnimation() {
+        console.log('normal win')
+        console.log("plays normal win animation")
+
+        // let container = new PIXI.ParticleContainer();
+
+        // for (let i = 0; i < 100; ++i)
+        // {
+        //     let sprite = PIXI.Sprite.from('images/logo.png');
+        //     container.addChild(sprite);
+        // }
+
+      }
+
+
+      playBonusWinAnimation() {
+        console.log('bonus win')
+        console.log('plays bonus win animation ')
+      }
     
+      playLoseAnimation() {
+        console.log("chosen chest is not a win");
+        
+      }
+
+
 }
